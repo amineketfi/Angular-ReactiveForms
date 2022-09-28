7d3505058611094413348e21e8431f8d855a70b1
amineketfi <amineketfi.cs50x@gmail.com>
2022-09-28 10:54:33 +0100

    custom validators

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+// // Custom validator
+// function ratingRange(c: AbstractControl): { [key: string]: boolean } | null {
+//   if(c.value !== null && (isNaN(c.value)  || c.value < 1 || c.value > 5))
+//     return { 'range': true };
+//   return null
+// }
+
+// Custom validator with params
+function ratingRange(min: number , max: number): ValidatorFn {  // ==> ratingRange func is a Factory Function
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if(c.value !== null && (isNaN(c.value)  || c.value < min || c.value > max))
+      return { 'range': true };
+    return null
+  }
+}
+
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +26,63 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    // Create FormGroup using FormBuilder
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.email, Validators.required]],
+      phone: '',
+      notification:'email',
+      rating: ['null', ratingRange(1, 5)],
+      sendCatalog: true
+    })
+
+    // // Create FormGroup
+    // this.customerForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true)
+    // })
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  // Func adjusts validators dynamicaly , invoked when notification radio buttons clicked
+  setNotification(ntotifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (ntotifyVia == 'text')
+      phoneControl.setValidators(Validators.required); // single validator; if multiple: []
+    else
+      phoneControl.clearValidators();
+    phoneControl.updateValueAndValidity(); // revaluate the phone formControl state of validation (essential)
+  }
+
+  // Populate test data
+  populateTestData() {
+
+    // Complete Fill
+    this.customerForm.setValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@example.com',
+      sendCatalog: false
+    })
+
+    // // Partial Fill
+    // this.customerForm.patchValue({
+    //   firstName: 'Jack',
+    //   email: 'jack@example.com',
+    // })
   }
 }
